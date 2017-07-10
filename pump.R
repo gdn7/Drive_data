fd865450bf68077395b68f208e1b2872c797b6ea
Niranjan GD <niranj@okstate.edu>
2017-07-10 05:42:36 -0700

    Update pump.R

--- pump.R
+++ pump.R
@@ -1,12 +1,1 @@
-library(foreign)
-library(MASS)
-library(nnet)
-getwd()
-setwd("C:\\Users\\DELL\\Desktop\\Drive_Data")
-
-pump=read.csv(file="Book1.csv")
-
-
-model = nnet(pump,size=10, linout)
-str(pump)
 
