603e7aa23d40522266899a5a7005c59c8bbbe4cf
Niranjan GD <niranj@okstate.edu>
2017-08-08 21:05:33 -0700

    Update data_driven.py

--- data_driven.py
+++ data_driven.py
@@ -6,12 +6,10 @@ import matplotlib.pyplot as plt
 import os
 
 
-
 #data Pre processing
 driven_data=pd.read_csv("C:\\Users\\gdnir\\Desktop\\OSU_Dec\\Transfer_dec_'16-20161215T045007Z\Transfer_dec_'16\\Drive_Data\\Train.csv")
 
 driven_data_clean=driven_data.drop(['date_recorded','funder','installer','wpt_name','subvillage','region','region_code','district_code','lga','ward','scheme_name','quantity','source','waterpoint_type_group'] ,axis=1)
-
 driven_data_clean.head(5)
 
 data=driven_data_clean.fillna(method='pad')
